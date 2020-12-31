2f12915529716e0c32c99f8641fcb06bc92c1d8a
Minty <kinostl@gmail.com>
2020-12-31 06:34:53 -0500

    Moved some stuff around for await purposes, and fixed the auth

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix, token, GOOGLE_API_KEY} = process.env;
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 const client = new Discord.Client();
 
-async function callGoogle(message, args) {
+async function getEntityInfo(channel, tab_name) {
+  /** get channel info */
+  /** get channels sheet */
+  const doc = new GoogleSpreadsheet("<the sheet ID from the url>");
+  await doc.useApiKey(GOOGLE_API_KEY);
+
+  await doc.loadInfo();
+  /** check if tab exists */
+  let db = doc.sheetsByTitle[tab_name];
+  db = await db.getRows();
+  /** check if row exists in tab */
+  let row = db.find((el) => {
+    el.name == entity_name;
+  });
+  let entity_details = Object.entries(row).map((col) => ({
+    name: col[0],
+    value: col[1],
+  }));
+  entity_details.shift();
+  return entity_details;
+}
+
+async function sendEntityInfo(message, args) {
   message.channel.startTyping();
   const tab_name = args.shift().toLowerCase();
   const entity_name = args.shift().toLowerCase();
-  const doc = new GoogleSpreadsheet("<the sheet ID from the url>");
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-  });
 
   try {
-    await doc.loadInfo();
-    /** check if tab exists */
-    let db = doc.sheetsByTitle[tab_name];
-    db = await db.getRows();
-    /** check if row exists in tab */
-    let row = db.find((el) => {
-      el.name == entity_name;
-    });
-    let entity_details = Object.entries(row).map((col) => ({
-      name: col[0],
-      value: col[1],
-    }));
-    entity_details.shift();
+    let entity_details = await getEntityInfo(message.channel, tab_name);
     /** output the embed */
     const embed = new Discord.MessageEmbed()
       .setTitle(entity_name)
@@ -36,8 +41,9 @@ async function callGoogle(message, args) {
   } catch (e) {
     /** let them know what didn't exist or that there might be a typo */
     await message.reply(e);
+  } finally {
+    message.channel.stopTyping();
   }
-  message.channel.stopTyping();
 }
 
 client.once("ready", () => {
@@ -48,7 +54,7 @@ client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
-  callGoogle(message, args);
+  sendEntityInfo(message, args);
 });
 
 client.login(token);
