cbe9e781005684ceff02b68847dd9b0da68abff8
Minty <kinostl@gmail.com>
2020-12-31 07:00:12 -0500

    Other small improvements

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ async function getEntityInfo(channel, tab_name) {
   /** get channel info */
   /** get channels sheet */
   const doc = new GoogleSpreadsheet("<the sheet ID from the url>");
-  await doc.useApiKey(GOOGLE_API_KEY);
-
+  doc.useApiKey(GOOGLE_API_KEY);
   await doc.loadInfo();
+
   /** check if tab exists */
   let db = doc.sheetsByTitle[tab_name];
   db = await db.getRows();
+
   /** check if row exists in tab */
   let row = db.find((el) => {
     el.name.toLowerCase().startsWith(entity_name);
   });
+
   let entity_details = Object.entries(row).map((col) => ({
     name: col[0],
     value: col[1],
   }));
   entity_details.shift();
+
   return entity_details;
 }
 
@@ -32,11 +35,13 @@ async function sendEntityInfo(message, args) {
   const entity_name = args.shift().toLowerCase();
 
   try {
-    let entity_details = await getEntityInfo(message.channel, tab_name, entity_name);
+	let entity_details = await getEntityInfo(message.channel, tab_name, entity_name);
+
     /** output the embed */
     const embed = new Discord.MessageEmbed()
       .setTitle(entity_name)
-      .addFields(entity_details);
+	  .addFields(entity_details);
+
     await message.reply(embed);
   } catch (e) {
     /** let them know what didn't exist or that there might be a typo */
